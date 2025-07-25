f3ab952bc5933bc544615f70cec610ab631c33a1
darizae <danielarizae@gmail.com>
2025-07-25 12:51:34 +0200

    Work book one

---
+++ 01. intro-to-springboot/workbook-one/src/main/java/com/ltp_one/workbook_one/WorkbookOneApplication.java
@@ -0,0 +1,14 @@
+package com.ltp_one.workbook_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbookOneApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		SpringApplication.run(WorkbookOneApplication.class, args);
+	}
+
+}
---
+++ 01. intro-to-springboot/workbook-one/src/test/java/com/ltp_one/workbook_one/WorkbookOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltp_one.workbook_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkbookOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
